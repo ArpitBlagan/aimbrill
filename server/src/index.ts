7e1b29b9167f04de63889748bd578787fd3b2a5a
arpit <blaganarpit@gmail.com>
2024-04-05 11:31:04 +0530

    done

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,7 +21,12 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://aimbrill-zaz3.vercel.app", "*"],
+    origin: [
+      "http://localhost:5173",
+      "https://aimbrill-zaz3.vercel.app",
+      "https://aimbrill.vercel.app/",
+      "*",
+    ],
     credentials: true,
   })
 );
